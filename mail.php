70db1dfebb3356bcf5a92f89ecd379d08f05f0b5
KatrinAnoshka <nosha309@mail.ru>
2018-05-13 20:44:19 +0300

    fixed mail.php

--- mail.php
+++ mail.php
@@ -1,19 +1,22 @@
 <?php
 
-$frm_name  = "Заявка!";
-$recepient = "melana309@gmail.com, nosha309@mail.ru, nosha_bobruysk@mail.ru";
-$sitename  = "Ремонт компьютеров в Бобруйске";
-$subject   = "Новая заявка с сайта \"$sitename\"";
+$frm_name  = "Message!";
+$recepient = "melana309@gmail.com, nosha309@mail.ru, nosha_bobruysk@mail.ru, katrinanoshka@gmail.com";
+$sitename  = "Katrin Anoshka Portfolio";
+$subject   = "Message from website \"$sitename\"";
 $email     = "info@bobrmama.by";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
-$formname = trim($_POST["formname"]);
+$mail = trim($_POST["mail"]);
+$text = trim($_POST["text"]);
+// $formname = trim($_POST["formname"]);
 
 $message = "
-Форма: $formname <br>
-Имя: $name <br>
-Телефон: $phone
+Name: $name <br>
+Phone: $phone <br>
+Mail: $mail <br>
+Text: $text
 ";
 
 mail($recepient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
